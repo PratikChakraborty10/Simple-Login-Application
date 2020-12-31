f3b9e2912dec701e247fefed79523dc27242e316
PratikChakraborty10 <chakrabortypratik2000@gmail.com>
2020-12-31 15:47:07 +0530

    minor changes in design

--- app/src/main/java/com/example/login/SignUpActivity.kt
+++ app/src/main/java/com/example/login/SignUpActivity.kt
@@ -17,6 +17,7 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        supportActionBar?.hide()
         auth = FirebaseAuth.getInstance()
         //auth = Firebase.auth
 
