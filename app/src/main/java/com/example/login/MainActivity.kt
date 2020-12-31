1d46f76eb8e63dee75b9366cf554080aff271364
PratikChakraborty10 <chakrabortypratik2000@gmail.com>
2020-12-31 12:40:14 +0530

    Sign Up Bug Error Solved

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,11 +19,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         auth = FirebaseAuth.getInstance()
 
-        val signup = findViewById<Button>(R.id.signup)
+        //val signup = findViewById<Button>(R.id.signup)
+
+
         signup.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
         }
-
+        /*fun open_signup(view: View) {
+            startActivity(Intent(this, SignUpActivity::class.java))
+            finish()
+        }*/
 
     }
 
@@ -35,5 +42,11 @@ class MainActivity : AppCompatActivity() {
     fun updateUI(currentUser: FirebaseUser?) {
 
     }
+
+//    fun open_signup(view: View) {
+//        startActivity(Intent(this, SignUpActivity::class.java))
+//        finish()
+//    }
+
 
 }
--- app/src/main/java/com/example/login/SignUpActivity.kt
+++ app/src/main/java/com/example/login/SignUpActivity.kt
@@ -7,7 +7,10 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+//import kotlinx.android.synthetic.main.activity_main.*
+
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -15,33 +18,37 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
         auth = FirebaseAuth.getInstance()
+        //auth = Firebase.auth
 
-        val signupBt = findViewById<Button>(R.id.sign_up_bt)
 
-        signupBt.setOnClickListener {
+         //var signupBt = findViewById<Button>(R.id.sign_up_bt_sign)
+
+        sign_up_bt_sign.setOnClickListener {
             signUpUser()
         }
+
+
     }
-    val tv_username = findViewById<EditText>(R.id.email_et)
-    val tv_pass = findViewById<EditText>(R.id.pass_et)
+    //var tv_username = findViewById<EditText>(R.id.email_et)
+    //var tv_pass = findViewById<EditText>(R.id.pass_et)
     private fun signUpUser() {
-        if(tv_username.text.toString().isEmpty()) {
-            tv_username.error = "Please enter email ID"
-            tv_username.requestFocus()
+        if(email_et.text.toString().isEmpty()) {
+            email_et.error = "Please enter email ID"
+            email_et.requestFocus()
             return
         }
-        if(!Patterns.EMAIL_ADDRESS.matcher(tv_username.text.toString()).matches()) {
-            tv_username.error = "Please enter a valid Emain ID"
-            tv_username.requestFocus()
+        if(!Patterns.EMAIL_ADDRESS.matcher(email_et.text.toString()).matches()) {
+            email_et.error = "Please enter a valid Email ID"
+            email_et.requestFocus()
             return
         }
-        if(tv_pass.text.toString().isEmpty()) {
-            tv_pass.error = "Please enter password"
-            tv_pass.requestFocus()
+        if(pass_et.text.toString().isEmpty()) {
+            pass_et.error = "Please enter password"
+            pass_et.requestFocus()
             return
         }
 
-        auth.createUserWithEmailAndPassword(tv_username.text.toString(), tv_pass.text.toString())
+        auth.createUserWithEmailAndPassword(email_et.text.toString(), pass_et.text.toString())
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
