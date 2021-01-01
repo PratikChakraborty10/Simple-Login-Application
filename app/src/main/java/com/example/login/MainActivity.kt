1801ec4eecb0714aa3390fb3bd21fb9561076efd
PratikChakraborty10 <chakrabortypratik2000@gmail.com>
2021-01-01 11:36:07 +0530

    bug fix

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.example.login
 
+
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+//import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Patterns
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.email_et
-import kotlinx.android.synthetic.main.activity_main.pass_et
-import kotlinx.android.synthetic.main.activity_sign_up.*
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,60 +17,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /*window.decorView.apply {
-            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
-        }*/
-        supportActionBar?.hide()
         auth = FirebaseAuth.getInstance()
 
-        //val signup = findViewById<Button>(R.id.signup)
-
-
         signup.setOnClickListener {
-            startActivity(Intent(this, SignUpActivity::class.java))
-        }
-
-        login_bt.setOnClickListener {
-           doLogin()
-        }
-
-        /*fun open_signup(view: View) {
-            startActivity(Intent(this, SignUpActivity::class.java))
+            startActivity(Intent(this,SignUpActivity::class.java))
             finish()
-        }*/
+        }
 
     }
-
-    private fun doLogin() {
-        if(email_et.text.toString().isEmpty()) {
-            email_et.error = "Please enter email ID"
-            email_et.requestFocus()
-            return
-        }
-        if(!Patterns.EMAIL_ADDRESS.matcher(email_et.text.toString()).matches()) {
-            email_et.error = "Please enter a valid Email ID"
-            email_et.requestFocus()
-            return
-        }
-        if(pass_et.text.toString().isEmpty()) {
-            pass_et.error = "Please enter password"
-            pass_et.requestFocus()
-            return
-        }
-        auth.signInWithEmailAndPassword(email_et.text.toString(), pass_et.text.toString())
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        val user = auth.currentUser
-                        updateUI(user)
-                    } else {
-                        Toast.makeText(baseContext, "Authentication failed.",
-                                Toast.LENGTH_SHORT).show()
-                        updateUI(null)
-                    }
-                }
-    }
-
-
 
     public override fun onStart() {
         super.onStart()
@@ -87,11 +36,6 @@ class MainActivity : AppCompatActivity() {
     fun updateUI(currentUser: FirebaseUser?) {
 
     }
-
-//    fun open_signup(view: View) {
-//        startActivity(Intent(this, SignUpActivity::class.java))
-//        finish()
-//    }
 
 
 }
--- app/src/main/java/com/example/login/SignUpActivity.kt
+++ app/src/main/java/com/example/login/SignUpActivity.kt
@@ -1,73 +1,59 @@
 package com.example.login
 
+
 import android.content.Intent
+//import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-//import kotlinx.android.synthetic.main.activity_main.*
-
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
+
     private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-        /*window.decorView.apply {
-            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
-        }*/
-        
-        supportActionBar?.hide()
         auth = FirebaseAuth.getInstance()
-        //auth = Firebase.auth
-
-
-         //var signupBt = findViewById<Button>(R.id.sign_up_bt_sign)
 
         sign_up_bt_sign.setOnClickListener {
             signUpUser()
         }
 
-
     }
-    //var tv_username = findViewById<EditText>(R.id.email_et)
-    //var tv_pass = findViewById<EditText>(R.id.pass_et)
+
     private fun signUpUser() {
-        if(email_et.text.toString().isEmpty()) {
-            email_et.error = "Please enter email ID"
+        if (email_et.text.toString().isEmpty()) {
+            email_et.error = "Please enter email"
             email_et.requestFocus()
             return
         }
-        if(!Patterns.EMAIL_ADDRESS.matcher(email_et.text.toString()).matches()) {
-            email_et.error = "Please enter a valid Email ID"
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email_et.text.toString()).matches()) {
+            email_et.error = "Please enter valid email"
             email_et.requestFocus()
             return
         }
-        if(pass_et.text.toString().isEmpty()) {
+
+        if (pass_et.text.toString().isEmpty()) {
             pass_et.error = "Please enter password"
             pass_et.requestFocus()
             return
         }
 
         auth.createUserWithEmailAndPassword(email_et.text.toString(), pass_et.text.toString())
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    startActivity(Intent(this, MainActivity::class.java))
-                    finish()
-
-                } else {
-                    Toast.makeText(baseContext, "Sign Up Failed. Try again after some time.",
-                        Toast.LENGTH_SHORT).show()
-
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    } else {
+                        Toast.makeText(baseContext, "Sign Up failed. Try again after some time.",
+                                Toast.LENGTH_SHORT).show()
+                    }
                 }
-
-            }
-
     }
 }
